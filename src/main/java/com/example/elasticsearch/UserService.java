744e1cf3d5ea31a5ef88e7c193f12e36a7c1746d
justgotothedesk <wlscjf3877@naver.com>
2024-01-27 16:36:21 +0900

    User 서비스 추가

---
+++ src/main/java/com/example/elasticsearch/UserService.java
@@ -0,0 +1,27 @@
+package com.example.elasticsearch;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public void deleteUser(User user) {
+        userRepository.delete(user);
+    }
+
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+}
