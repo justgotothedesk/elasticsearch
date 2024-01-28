8410dbe0503655a81e26a73746d04d0c092024e3
justgotothedesk <wlscjf3877@naver.com>
2024-01-28 17:30:49 +0900

    포스트맨을 통한 api 조회 컨트롤러 추가

---
+++ src/main/java/com/example/elasticsearch/UserApiController.java
@@ -0,0 +1,26 @@
+package com.example.elasticsearch;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserApiController {
+
+    private final UserService userService;
+
+    public UserApiController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/find")
+    public List<User> findUser(@RequestParam String name) {
+        List<User> users = userService.getUserByName(name);
+
+        return users;
+    }
+}
