842ea169bfe47b263c87223c340b611b51ef347d
justgotothedesk <wlscjf3877@naver.com>
2024-01-28 17:30:07 +0900

    이름 검색 메서드 추가

--- src/main/java/com/example/elasticsearch/UserRepository.java
+++ src/main/java/com/example/elasticsearch/UserRepository.java
@@ -1,8 +1,14 @@
 package com.example.elasticsearch;
 
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends ElasticsearchRepository<User, String> {
+    List<User> findByName(String name);
+
 }
